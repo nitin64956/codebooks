e507f75348fbd029efd04bd709388962cdb0fcea
nitin verma <nitin64956@gmail.com>
2023-10-09 19:40:27 +0530

    frontend deployment

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,jsx,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        dark: "#1E293B",
+      }
+    },
+  },
+  plugins: [],
+}
